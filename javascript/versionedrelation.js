3f022b7d086e42eaa8630e3615003809b27a653e
Serafino Donato Fornito <sdf@flxlabs.com>
2017-04-11 12:33:05 +0200

    fix(Page&Element load): fixed an issue in which you couldn’t use the CMS properly

--- javascript/versionedrelation.js
+++ javascript/versionedrelation.js
@@ -1,16 +1,18 @@
 (function($) {
     $( document ).ajaxComplete(function( event, xhr, settings ) {
-        if (xhr.responseText) {
-            $response = $(xhr.responseText);
-            var $divElem = $response.filter("#reorder-happened");
-            $('body').append($divElem);
+        if (xhr.responseText && typeof(xhr.responseText) === "string") {
+            if(xhr.responseText.indexOf("reorder-happened") !== -1) {
+                $response = $(xhr.responseText);
+                var $divElem = $response.filter("#reorder-happened");
+                $('body').append($divElem);
 
-            if ($divElem.length){
-                $('.cms-edit-form .Actions #Form_EditForm_action_publish').button({
-                    showingAlternate: true
-                });
-                $("#record-" + $divElem.data("object-id")).addClass("status-modified");
-                $( document ).remove("#reorder-happened");
+                if ($divElem.length){
+                    $('.cms-edit-form .Actions #Form_EditForm_action_publish').button({
+                        showingAlternate: true
+                    });
+                    $("#record-" + $divElem.data("object-id")).addClass("status-modified");
+                    $( document ).remove("#reorder-happened");
+                }
             }
         }
     });
